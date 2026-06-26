6e5c96a7baf0a645823843748f5e62aeb568461e
Muller Hugo <hugomuller@MacBook-Air-de-Muller.local>
2026-06-26 18:58:57 +0200

    fix: prisma generate in build + fix client import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: InstanceType<typeof PrismaClient> };
 
 function createPrismaClient() {
   const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
-  return new PrismaClient({ adapter });
+  return new PrismaClient({ adapter } as any);
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
